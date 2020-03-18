bdfdd3b4e42e3477c7cc5130018ce1d927cb2e41
Gautam Gopinathan <56235768+gotumjee@users.noreply.github.com>
2020-03-18 14:02:56 +0000

    Update network_example.py

--- network_example.py
+++ network_example.py
@@ -2,7 +2,7 @@ from network import *
 
 #this file is just an example of how the network stuff will be used
 #one client will call host(), and the other will call connect()
-#to send a string, one client must be listening using reveive() and then the other calls send
+#to send a string, one client must be listening using receive() and then the other calls send
 #at the end, both clients should call close()
 
 network = Network_sockets()
