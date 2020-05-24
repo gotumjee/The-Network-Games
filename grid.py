e19a08901923d274a7eeb7055655d292efea308d
james-wrench <61830381+james-wrench@users.noreply.github.com>
2020-05-24 22:28:27 +0100

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,30 +1,53 @@
-### TODO Error Checking
+class Grid:
+    def __init__(self, x, y, fill=None):
+        if isinstance(x, int) is not True or isinstance(y, int) is not True:
+            raise TypeError("Grid x and y values must be integers")
+        if x <= 0 or y <= 0:
+            raise TypeError("Grid x and y values must be greater than zero")
 
-class Grid():
-    def __init__(self, x, y, fill = None):
         self.x = x
         self.y = y
+        self.grid = []
         self.setGridValues(fill)
-        
+
     def setCellValue(self, x, y, value):
+        if isinstance(x, int) is not True or isinstance(y, int) is not True:
+            raise TypeError("Grid x and y values must be integers")
+
         self.grid[x][y] = value
 
     def getCellValue(self, x, y):
         return self.grid[x][y]
 
-    def setXStripValues(self, x, value):
-        for n in range(self.y):
+    def setXStripValues(self, x, value, set_independent=0):
+        if set_independent:
+            for n in range(self.y):
+                self.grid[x][n] = value[n]
+        else:
+            for n in range(self.y):
                 self.grid[x][n] = value
 
-    def setYStripValues(self, y, value):
-        for n in range(self.x):
+    def setYStripValues(self, y, value, set_independent=0):
+        if set_independent:
+            for n in range(self.x):
+                self.grid[n][y] = value[n]
+
+        else:
+            for n in range(self.x):
                 self.grid[n][y] = value
 
+    def findPiece(self, value):
+        for i in range(self.x):
+            for j in range(self.y):
+                if self.grid[i][j] == value:
+                    return [i, j]
+        return None
+
     def setGridValues(self, value):
-        self.grid = [[value for i in range(self.y)] for j in range(self.x)]
+        self.grid = [[value for _ in range(self.y)] for __ in range(self.x)]
 
     def swapTwo(self, x1, y1, x2, y2):
-        temp = self.getCellValue(x1,y1)
-        
-        self.setCellValue(x1, y1, self.getCellValue(x2,y2))
+        temp = self.grid[x1][y1]
+
+        self.setCellValue(x1, y1, self.grid[x2][y2])
         self.setCellValue(x2, y2, temp)
