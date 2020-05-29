9989e1915de1e57ff38f541a269603b0216b027a
james-wrench <61830381+james-wrench@users.noreply.github.com>
2020-05-29 20:40:44 +0100

    Update network.py
    Altered to conform to PEP-8 (except for the class name)

--- network.py
+++ network.py
@@ -1,58 +1,66 @@
 import socket
-from Crypto.Cipher import AES #use "pip install pycryptodome" to install the required libraries
+from Crypto.Cipher import AES # Use "pip install pycryptodome" to install the required libraries
 
-#implementing symmetric encryption with both parties having to use the same key and initalization vector
+
+# Implementing symmetric encryption with both parties having to use the same key and initalization vector
 def do_encrypt(message):
-    obj = AES.new("#carperiemdabest".encode("utf8"), AES.MODE_CFB, "onlysometimestho".encode("utf8")) #creates an object (cipher) to encrypt the message
-    message = message.encode("utf8") #converts the string to binary to be encrypted
-    ciphertext = obj.encrypt(message) #encrypts the message using the unique cipher
-    return ciphertext
+    # Creates an object (cipher) to encrypt the message
+    obj = AES.new("#carperiemdabest".encode("utf8"), AES.MODE_CFB, "onlysometimestho".encode("utf8"))
+    # Converts the string to binary to be encrypted
+    message = message.encode("utf8")
+    # Encrypts the message using the unique cipher
+    cipherText = obj.encrypt(message) 
+    return cipherText
 
-def do_decrypt(ciphertext):
-    obj2 = AES.new("#carperiemdabest".encode("utf8"), AES.MODE_CFB, "onlysometimestho".encode("utf8")) #creates an object (cipher) to decrypt the message
-    message = obj2.decrypt(ciphertext) #converts the string to binary to be decrypted
-    message = message.decode("utf8") #decrypts the message using the unique cipher
+
+def do_decrypt(cipherText):
+    # Creates an object (cipher) to decrypt the message
+    obj2 = AES.new("#carperiemdabest".encode("utf8"), AES.MODE_CFB, "onlysometimestho".encode("utf8"))
+    # Converts the string to binary to be decrypted
+    message = obj2.decrypt(cipherText)
+    # Decrypts the message using the unique cipher
+    message = message.decode("utf8") 
     return message
+
 
 class Network_sockets:
     def __init__(self, port=6969):
-        #takes port number
-        #define port and use TCP
-        #use 6969 if no port number is given
-        #if number too high, use default
-        if(port > 65535):
+        # Takes port number
+        # Define port and use TCP
+        # Use 6969 if no port number is given
+        # If number too high, use default
+        if port > 65535:
             port = 6969
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.hosting = False
+        self.conn = None
+        self.addr = None
 
     def host(self):
         # Wait for incoming connection
         self.hosting = True
-        self.sock.bind(('',self.port))
+        self.sock.bind(('', self.port))
         self.sock.listen(5)
         self.conn, self.addr = self.sock.accept()
-        
-        # Debug Code
-        # print("Received a connection from ", self.addr)
 
     def connect(self, ip_addr):
         self.sock.connect((ip_addr, self.port))
 
-    def send(self, msg = ''):
-        #takes a string then sends
-        msg=do_encrypt(str(msg))
-        if(self.hosting):
+    def send(self, msg=''):
+        # Takes a string then sends
+        msg = do_encrypt(str(msg))
+        if self.hosting:
             self.conn.send(msg)
         else:
             self.sock.send(msg)
 
     def receive(self):
-        #wait for the return string
-        if(self.hosting):
-            return(do_decrypt(self.conn.recv(1024)))
-        return(do_decrypt(self.sock.recv(1024)))
+        # Wait for the return string
+        if self.hosting:
+            return do_decrypt(self.conn.recv(1024))
+        return do_decrypt(self.sock.recv(1024))
 
     def close(self):
-        #close connection
+        # Close connection
         self.sock.close()
